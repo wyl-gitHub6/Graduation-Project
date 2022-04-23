e2c7157b5791600f6ca3e7ecfc3db9677ff45a16
wangyl <wyl_66666@163.com>
2022-04-23 20:42:41 +0800

    nginx反向代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 /*
- * @Description: 
+ * @Description: 跨域配置
  * @Author: Wangyl
  * @Date: 2021-10-05 17:50:03
  * @LastEditors: Wangyl
@@ -12,8 +12,9 @@ module.exports = {
     port: 1234,   //更改自己前端项目端口
     proxy: {      //设置代理，必须填
       '/api': {  //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
-        target: 'http://localhost:8080',     //代理的目标地址
-        //target: http://192.168.153.133:9000,     //代理的目标地址 docker部署地址
+        //target: 'http://localhost:8080',          //代理的目标地址
+        //target: http://192.168.153.133:9000,      //代理的目标地址 docker部署地址
+        target: "http://score.system.com",          //nginx反向代理地址
         changeOrigin: true,              //是否设置同源，输入是的
         pathRewrite: {                   //路径重写
           '/api': ''                     //选择忽略拦截器里面的单词
